df21cd31db47ab7d2640bf391c5d00b18aa173fe
alice <zhupanova2017sv@mail.ru>
2023-01-29 19:13:15 +0300

    task1

---
+++ OneHW/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+
+int num = new Random().Next(10000, 100000);
+int rev = 0;
+
+while (num > 0)
+{
+    int lost = num % 10;
+    rev= (rev * 10) + lost;
+    num = num / 10;
+}
+if (num == rev)
+{
+    System.Console.WriteLine($"{rev} = палиндром!");
+}
+else
+{
+    System.Console.WriteLine($"{rev} = НЕ палиндром!");
+}
